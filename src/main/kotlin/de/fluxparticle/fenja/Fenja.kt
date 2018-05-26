2912bbba0d1c57812265fbf9ba7ad8d0658d3624
Sven Reinck <sreinck@fluxparticle.de>
2018-05-26 17:06:23 +0200

    ein paar ValueLifter in Kotlin, damit das kombinieren von value auch in Kotlin gut funktioniert

--- src/main/kotlin/de/fluxparticle/fenja/Fenja.kt
+++ src/main/kotlin/de/fluxparticle/fenja/Fenja.kt
@@ -21,3 +21,22 @@ fun fenjaBuilder(block: FenjaBuilder.() -> Unit) {
 infix fun <T> Property<T>.bindTo(observableValue: ObservableValue<out T>) {
     return this.bind(observableValue)
 }
+
+operator fun <T, U> Value<T>.times(other: Value<U>): ValueLifter2<T, U> {
+    return ValueLifter2(this, other)
+}
+
+class ValueLifter2<T, U>(private val param1: Value<T>, private val param2: Value<U>) {
+    operator fun <R> invoke(block: (T, U) -> R): Value<R> {
+        return param1.lift(param2, block)
+    }
+    operator fun <V> times(other: Value<V>): ValueLifter3<T, U, V> {
+        return ValueLifter3(param1, param2, other)
+    }
+}
+
+class ValueLifter3<T, U, V>(private val param1: Value<T>, private val param2: Value<U>, private val param3: Value<V>) {
+    operator fun <R> invoke(block: (T, U, V) -> R): Value<R> {
+        return param1.lift(param2, param3, block)
+    }
+}
